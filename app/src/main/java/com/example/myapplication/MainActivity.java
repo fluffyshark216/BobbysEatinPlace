a507f1debb0217d660c586bde17ce7959320b795
lf0898278 <fluffyshark216@gmail.com>
2023-03-29 10:06:02 -0500

    Prefs from main activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -51,5 +52,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, ThankYou.class));
             }
         });
+       SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs",MODE_PRIVATE);
+       SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString("email",String.valueOf(R.id.txtEmail));
+        editor.putString("email",String.valueOf(R.id.txtPhone));
+        editor.putString("email",String.valueOf(R.id.edtName));
+        editor.putString("email",String.valueOf(R.id.edtPartySize));
+        editor.apply();
     }
 }
